926b8b3656a35649160147355a0644c21ec044b0
Evil eye <soni.rishi16@outlook.com>
2024-04-20 21:58:56 -0500

    commit

---
+++ dist/my-angular-project/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-DMI4UO2I.mjs";import"./chunk-3FQMTSHA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ src/app/port/port/port.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-port',
+  standalone: true,
+  imports: [],
+  templateUrl: './port.component.html',
+  styleUrl: './port.component.css'
+})
+export class PortComponent {
+
+}
